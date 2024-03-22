b4fe90541147d8245598bb70f006546adada8db5
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2024-03-22 16:49:01 +0000

    Add files via upload

---
+++ 02. Primitive Data Types.js
@@ -0,0 +1,20 @@
+// String with double quotes
+console.log("This is a string");
+
+// String with single quotes
+console.log('This is also a string');
+
+// Number
+console.log(1)
+
+// BigInt
+console.log(1234567890123456n);
+
+//Boolean
+console.log(true);
+
+//Null
+console.log(null);
+
+//Undefined
+console.log(undefined);
