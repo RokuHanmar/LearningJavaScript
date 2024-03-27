883d36b913478f5ac16940ae482ba2a02c3393fd
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2024-03-27 17:03:50 +0000

    Update and rename 08. Variables.js to 08. Variables and Constants.js
    Added variables created with 'let' keyword and constants to this file

---
+++ 08. Variables and Constants.js
@@ -0,0 +1,14 @@
+var name = 'Rohan';
+var age = 19;
+
+console.log(name);
+console.log(age);
+
+
+let changeMe = true;
+
+changeMe = false;
+console.log(changeMe);
+
+const answer = 42;
+console.log(answer)
--- 08. Variables.js
+++
@@ -1,5 +0,0 @@
-var name = 'Rohan';
-var age = 19;
-
-console.log(name);
-console.log(age);
