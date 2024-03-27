2d3a979c7c199ec0f93bf1785edd124ceb1d6ac0
RokuHanmar <90313219+RokuHanmar@users.noreply.github.com>
2024-03-27 16:48:45 +0000

    Create 06. String Methods.js

---
+++ 06. String Methods.js
@@ -0,0 +1,5 @@
+console.log('vengeance'.toUpperCase()); // returns 'VENGEANCE'
+
+console.log('WHISPER'.toLowerCase()); // returns 'whisper'
+
+console.log('                 Trimmed             '.trim()); // returns 'Trimmed'
